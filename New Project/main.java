399c98b0b0e59383028f8865485679ec70386495
nikabc <nikabc555@ya.ru>
2015-08-05 10:21:15 +0300

    Created by Coding Ground Client

---
+++ New Project/main.java
@@ -0,0 +1,10 @@
+public class main
+{
+     public static void main(String []args){
+        System.out.println("Hello World");
+        inputCollector ic = new inputCollector();
+        inputInfo info = new inputInfo();
+        info.test_integer = 6;
+        ic.doInput(info);
+     }
+}
