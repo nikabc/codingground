02babd214978813f505c2c9a84413298f6fc4204
nikabc <nikabc555@ya.ru>
2015-08-05 10:21:14 +0300

    Created by Coding Ground Client

---
+++ New Project/inputCollector.java
@@ -0,0 +1,13 @@
+class inputInfo
+{
+    public int test_integer;
+}
+
+class inputCollector
+{
+    public void doInput(inputInfo info)
+    {
+        System.out.println("doInput");
+        System.out.println(info.test_integer);
+    }
+}
